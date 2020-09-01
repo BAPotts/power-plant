cc39dd855a05039236550061dfdb1c94675b090c
Beverly Potts <pottsbeverly@gmail.com>
2020-09-01 17:03:48 -0400

    Add basic functionality to add and display soil level for one plant
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { stateControl } from './power-plant.js';
+import { blueFood } from './power-plant.js';
 import $ from "jquery";
 
 $(document).ready(function() {
+  $('#feed').click(function() {
+    const newState = stateControl(blueFood);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
+  });
 
+  $('#show-state').click(function() {
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  });
 });
--- src/power-plant.js
+++ src/power-plant.js
@@ -1,3 +1,63 @@
+
+
+// This function stores our state.
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+export const stateControl = storeState();
+
+// This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+
+const changeState = (prop) => {
+  return (value) =>{
+    return (state) => ({...state,
+      [prop]: (state[prop] || 0) + value})
+  }
+}
+
+// We create four functions using our function factory.
+
+export const feed = changeState("soil")(1);
+export const blueFood = changeState("soil")(5);
+export const hydrate = changeState("water")(1);
+export const superWater = changeState("water")(5);
+
+
+// UI Logic
+// $(document).ready(function() {
+//   $(form).submit(function(event) {
+//     event.preventDefault();
+
+//     $('#feed').click(function() {
+//       const newState = stateControl(blueFood);
+//       $('#soil-value').text(`Soil: ${newState.soil}`);
+//     });
+
+//     $('#show-state').click(function() {
+//       const currentState = stateControl();
+//       $('#soil-value').text(`Soil: ${currentState.soil}`);
+//     });
+//   });
+// });
+
+
+
+// let plant = {soil: 0, light: 0, water:0 }
+//console.log(changeState(plant, "soil"))
+// console.log(feed(5)(plant))
+// console.log(hydrate(3)(plant))
+// const greenFood = changeState("soil")(10);
+// const fedPlant = stateControl(blueFood);
+// const fedPlantAgain = stateControl(greenFood);
+
+
 // const hydrate = (plant) => {
 //   return {
 //     ...plant,
@@ -11,35 +71,3 @@
 //     soil: (plant.soil || 0) + 1
 //   }
 // };
-
-const changeState = (prop) => {
-  return (value) =>{
-    return (state) => ({...state, [prop]: (state[prop] || 0) + value})
-  }
-}
-
-const feed = changeState("soil");
-const hydrate = changeState("water");
-const giveLight = changeState("light");
-
-let plant = {soil: 0, light: 0, water:0 }
-//console.log(changeState(plant, "soil"))
-
-console.log(feed(5)(plant))
-console.log(hydrate(3)(plant))
-
-const blueFood = changeState("soil")(5);
-
-const storeState = () => {
-  let currentState = {};
-  return (stateChangeFunction) => {
-    const newState = stateChangeFunction(currentState);
-    currentState = {...newState};
-    return newState;
-  }
-}
-
-const stateControl = storeState();
-
-const fedPlant = stateControl(blueFood);
-console.log(fedPlant);
