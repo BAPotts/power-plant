023888b600b9381fbe8a4f25eb0801e5777feb3b
Beverly Potts <pottsbeverly@gmail.com>
2020-09-01 15:00:13 -0400

    experiment with state
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>

--- src/power-plant.js
+++ src/power-plant.js
@@ -1,6 +1,45 @@
-const hydrate = (plant) => {
-  return {
-    ...plant,
-    water: (plant.water || 0) + 1
+// const hydrate = (plant) => {
+//   return {
+//     ...plant,
+//     water: (plant.water || 0) + 1
+//   }
+// };
+
+// const feed = (plant) => {
+//   return {
+//     ...plant,
+//     soil: (plant.soil || 0) + 1
+//   }
+// };
+
+const changeState = (prop) => {
+  return (value) =>{
+    return (state) => ({...state, [prop]: (state[prop] || 0) + value})
   }
-};
+}
+
+const feed = changeState("soil");
+const hydrate = changeState("water");
+const giveLight = changeState("light");
+
+let plant = {soil: 0, light: 0, water:0 }
+//console.log(changeState(plant, "soil"))
+
+console.log(feed(5)(plant))
+console.log(hydrate(3)(plant))
+
+const blueFood = changeState("soil")(5);
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+const stateControl = storeState();
+
+const fedPlant = stateControl(blueFood);
+console.log(fedPlant);
